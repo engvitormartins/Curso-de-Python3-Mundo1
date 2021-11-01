479ca3a27fcc829c7931aedbf88dbec5ff8bb5c2
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-01 16:40:01 -0300

    Usando input para mostrar texto na tela

---
+++ Aulas/ex001.py
@@ -0,0 +1,5 @@
+#Vamos utilizar a função input para receber uma string que será mostrada.
+nome = input('Qual é seu nome? ')
+
+#Vamos utilizar a função "print" para mostrar o texto digitado pelo usuário na tela
+print('É um grande prazer te conhecer, ', nome+'!')
