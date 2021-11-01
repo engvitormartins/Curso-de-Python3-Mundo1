744226b14de2519fc661ba603e345cad38cc9b86
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-01 17:09:05 -0300

    Atualizando

---
+++ PythonExercicios/00-Hello-World.py
@@ -0,0 +1,8 @@
+# Comments
+
+# Comment is a text within a program that is not executed. It can ben used to add an information
+# to aid in understanding the code.
+
+# You can use for a single line or
+
+print("Hello world")    # Comment after code
