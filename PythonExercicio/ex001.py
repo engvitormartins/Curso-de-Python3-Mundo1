2f7e70b0096cee999d88006ed808dc3997e6cbba
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-01 18:07:46 -0300

    Exercício 01 com função IF, ELIF e ELSE

--- PythonExercicio/ex001.py
+++ PythonExercicio/ex001.py
@@ -5,3 +5,16 @@
 msg = 'Olá mundo!'  # O texto pode ser entre aspas simples ou dupla. Vamos sempre utiliza aspas simples.
 
 print(msg)  # Como eu criei uma variável, ela não precisará estar dentro das aspas, apenas dentro do ()
+
+
+
+# Criando um outro exercício utilizando If, Elif e Else
+
+var = int(input('Digite um número? '))
+
+if var > 0:
+    print(var,'é maior que Zero')
+elif var == 0:
+    print(var, 'é igual a Zero')
+else:
+    print(var, 'não é maior que Zero')
