6996d591cf33b54e17677cd0e6c75453591cabe4
Vitor Martins <93532494+engvitormartins@users.noreply.github.com>
2021-11-01 17:16:43 -0300

    Deletando

--- PythonExercicios/00-Hello-World.py
+++
@@ -1,8 +0,0 @@
-# Comments
-
-# Comment is a text within a program that is not executed. It can ben used to add an information
-# to aid in understanding the code.
-
-# You can use for a single line or
-
-print("Hello world")    # Comment after code
