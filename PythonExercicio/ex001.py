33e66f4fd2cfb002ddc958811bf734bf030903cb
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-01 17:42:51 -0300

    Atualizando Exercício 01 - Aula 04

---
+++ PythonExercicio/ex001.py
@@ -0,0 +1,7 @@
+# Aula 04 - Exercício 01
+
+# Criando uma variável para receber seu texto
+
+msg = 'Olá mundo!'  # O texto pode ser entre aspas simples ou dupla. Vamos sempre utiliza aspas simples.
+
+print(msg)  # Como eu criei uma variável, ela não precisará estar dentro das aspas, apenas dentro do ()
