4cf2dc8e305a3f25da5c23d8f71348b85c58925b
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-02 12:56:10 -0300

    Exercício 02

---
+++ PythonExercicio/ex002.py
@@ -0,0 +1,7 @@
+# Create a program that reads a person's name and display a welcome message
+
+# The "input()" function allows user input, and it will be representing a default message before the input
+name = input('Hello, typing your name: ')
+
+
+print(f'Hello {name}, Welcome our program!')
