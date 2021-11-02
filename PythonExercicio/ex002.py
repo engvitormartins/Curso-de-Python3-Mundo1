49169eb57e53be2e6f533d25bb235949f6445644
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-02 15:28:08 -0300

    Exercício 02 utilizando dicionários

--- PythonExercicio/ex002.py
+++ PythonExercicio/ex002.py
@@ -1,7 +1,25 @@
-# Create a program that reads a person's name and display a welcome message
+"""
+Create a program that reads a person's age for calculate the next Birthday and display it on the screen
+"""
 
 # The "input()" function allows user input, and it will be representing a default message before the input
-name = input('Hello, typing your name: ')
 
+# We're going to convert the user's age to an integer number using the "int()" function.
 
-print(f'Hello {name}, Welcome our program!')
+# Then, we'll add 1 (year) to that age.
+
+# On the final line of our code, we allow our value to print on the screen, alongside a short message.
+
+# Let's check the code in action:
+
+age = int(input('Hello, tell us your age: '))
+
+final_age = int(age + 1)
+
+print(f'\nOn your next Birthday, you will be: {final_age} years old! \n')
+
+ages = [age, final_age]
+
+ages_dictionary = dict.fromkeys(ages, "Age's Vitor")
+
+print(ages_dictionary)
