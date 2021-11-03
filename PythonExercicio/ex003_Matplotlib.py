e4198bba88913c12117889e3369e4bfc0acf32cc
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-03 08:36:17 -0300

    Exercício 03

--- PythonExercicio/ex003_Matplotlib.py
+++ PythonExercicio/ex003_Matplotlib.py
@@ -9,21 +9,22 @@ y = np.array([3, 8, 1, 10])
 
 plt.subplot(1, 2, 1)
 # plt.grid()
-plt.bar(x,y)
+plt.plot(x, marker = 'o', mfc = 'r')
 plt.title('SALES')
+
 
 #plot 2:
 x = np.array([5, 10, 50, 80])
 # y = np.array([10, 20, 30, 40])
 mylabels = ['ruim', 'medio', 'bom', 'excelente']
-myexplode = [0.05, 0.1, 0.15, 0.2]
+myexplode = [0, 0, 0, 0.2]
 mycolors = ['#C3C3C3', '#979797', '#787878', '#2C2B2B']
 
 
 
 plt.subplot(1, 2, 2)
 # plt.grid()
-plt.pie(x, labels = mylabels, explode=myexplode, shadow=True, colors=mycolors,
+plt.pie(x, labels = mylabels, explode=myexplode, shadow=False, colors=mycolors,
         autopct=lambda p:f'{p:.2f}%, {p*sum(x)/100 :.0f} items')
 
 # plt.legend(mylabels, loc = 'best', title = "Numbers of X:")
---
+++ PythonExercicio/ex004_Matplotlib.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import (MultipleLocator, AutoLocator)
+
+
+
+x = [1, 2, 3, 4]
+y = [1, 4, 2, 3]
+
+
+
+# plot01
+# plt.subplot(1, 2, 1)
+plt.plot(x, label='linear')
+plt.plot(y, label='quadratic')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title('Simple Plot')
+plt.legend()
+plt.minorticks_on()
+plt.tick_params(which='major', width=2)
+plt.tick_params(which='minor', length=4)
+plt.show()
+
+
+
